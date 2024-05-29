4cab47928a1bca9610d1d7b3e4ac755b18b36a40
Nurhadi10 <nurhadisabani9c@gmail.com>
2024-05-29 11:25:06 +0700

    commit pertanama

---
+++ src/helloworld.php
@@ -0,0 +1,10 @@
+<?php
+namespace hadie\libhallo;
+
+class helloworld{
+    public function sayHello($name){
+        return "hello $name welcome to php and composer to create your own library";
+    }
+}
+
+?>
